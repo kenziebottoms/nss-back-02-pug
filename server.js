084c7910d371b77100b0e8fd8d2239ca0b68443f
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-04-12 10:42:55 -0500

    basic index templating working

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 "use strict";
 
+const pug = require("pug");
 const express = require("express");
-
 const app = express();
 
+app.set("view engine", "pug");
+
 app.get("/", (req, res, next) => {
-    // homepage
+    res.render('index', { title: 'Hey', message: 'Hello there!' });
 });
 
 app.get("/about", (req, res, next) => {
@@ -14,4 +16,8 @@ app.get("/about", (req, res, next) => {
 
 app.get("/inventory", (req, res, next) => {
 
+});
+
+app.listen("8080", () => {
+    console.log(`Listening on port 8080`);
 });
